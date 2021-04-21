4277bccb00ac59f67d7a9bc247a1d3ff1f326f4d
Francesca Boccardi <s260186@studenti.polito.it>
2021-04-21 17:58:46 +0200

    Soluzione approccio 1

--- src/main/java/it/polito/tdp/ruzzle/model/Model.java
+++ src/main/java/it/polito/tdp/ruzzle/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.ruzzle.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.polito.tdp.ruzzle.db.DizionarioDAO;
@@ -46,13 +47,64 @@ public class Model {
 	}
 
 	public List<Pos> trovaParola(String parola) {
-		// TODO Auto-generated method stub
+		for(Pos p : board.getPositions()) {
+			if(board.getCellValueProperty(p).get().charAt(0)== parola.charAt(0)) {
+				List<Pos> percorso = new ArrayList<Pos>(); //tiene traccia delle posizioni delle lettere già riconosciute
+				percorso.add(p);
+				
+				if(cerca(parola,1,percorso)) { //parte da 1 perché la prima lettera è già stata aggiunta
+					return percorso;
+				}
+			}
+		}
+		
 		return null;
 	}
 
+	private boolean cerca(String parola, int livello, List<Pos> percorso) {
+		//caso terminale
+		
+		if(livello == parola.length()) {
+			return true;
+		}
+		
+		Pos ultima = percorso.get(percorso.size()-1);
+		List<Pos> adiacenti = board.getAdjacencies(ultima);
+		
+		for(Pos p : adiacenti) {
+			if(!percorso.contains(p) && parola.charAt(livello) == board.getCellValueProperty(p).get().charAt(0)) {
+				percorso.add(p);
+				
+				//faccio ricorsione
+				
+				//uscita rapida
+				if(cerca(parola,livello+1,percorso)) {
+					return true;
+				}
+				
+				//faccio backtracking solo quando non trovo effettivamente la parola e devo provare altri percorsi
+				
+				percorso.remove(percorso.size()-1);
+			}
+		}
+		
+		return false;
+	}
+
 	public List<String> trovaTutte() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<String> tutte = new ArrayList<String>();
+		
+		for(String parola : this.dizionario) {
+			
+			if(parola.length()>1) { //Ruzzle non accetta parole di una sola lettera
+				if(this.trovaParola(parola.toUpperCase())!=null) {
+					tutte.add(parola);
+				}
+			}
+		}
+		
+		return tutte;
 	}
 	
 
